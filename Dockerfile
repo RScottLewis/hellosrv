370e8a2cf2bcdda9fe02ff8918ee53a55046a26a
Roger Lewis <rogerlewis@Rogers-MacBook-Pro.local>
2018-03-02 14:58:09 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM golang:1.9-stretch
+MAINTAINER r.scott.lewis@gmail.com
+
---
+++ hellosrv.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func main() {
+		http.HandleFunc("/",index)
+		http.ListenAndServe(":80",nil)
+
+	}
+
+func index(w http.ResponseWriter, r *http.Request){
+	io.WriteString("hello from scott container")
+}
